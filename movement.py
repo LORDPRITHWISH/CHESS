0cb337af0133559f6d1e61fb7027426ecd3b8187
Prithwish Chakraborty <prithwishchakaborty05@gmail.com>
2024-01-10 17:06:05 +0530

    update

--- movement.py
+++ movement.py
@@ -4,7 +4,16 @@ def posible(x,y,t):
     print(*t,sep='\n')
     v=t[x][y]
     print(v)
+    
     if v==1 :
+        if t[x+1][y]==0:
+            out.append((x+1,y))
+        if t[x+1][y+1]<0 :
+            out.append((x+1,y+1))
+        if t[x+1][y-1]<0 :
+            out.append((x+1,y-1))
+        return out
+    elif v==-1 :
         if t[x+1][y]==0:
             out.append((x+1,y))
         if t[x+1][y+1]<0 :
@@ -15,4 +24,4 @@ def posible(x,y,t):
     
     
 ta=[[5, 4, 3, 20, 9, 3, 4, 5], [1, 1, 1, 1, 1, 1, 1, 1], [-1, 0, -1, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], [-1, -1, -1, -1, -1, -1, -1, -1], [-5, -4, -3, -20, -9, -3, -4, -5]]    
-print(posible(1,6,ta))
+print(posible(1,1,ta))
